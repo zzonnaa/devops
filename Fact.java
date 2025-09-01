32f31eece1647a845d047dee7cf52a820446cb34
zzonnaa <152791143+zzonnaa@users.noreply.github.com>
2025-09-01 14:15:06 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,14 @@
+public class Fact{
+  public static long compute(long n){
+    if(n<0) throw new IllegalArgumentException("Negative numbers not allowed");
+    int re=0;
+    for(int i=2;i<=n;i++){
+      re*=i;
+    }
+    return re;
+  }
+  public static void main(String[] args){
+    long n=5;
+    System.out.println("Factorial of"+n+"is: "+compute(n));
+  }
+}
